d2d1a997758e9802e842212b47f621282cc5bce3
mozart98 <mozart.assila@gmail.com>
2020-08-06 10:10:37 +0100

    Adding The repository bean

---
+++ src/app/beans/repository.ts
@@ -0,0 +1,6 @@
+export class Repository {
+    constructor(public RepositoryName: string, public RepositoryDescription: string,
+                public OwnerName: string, public OwnerAvatarPath: string,
+                public Nbstars: number, public Nbissues: number) {}
+  
+  }
